91ff1f3f8e243ee68c5e9b73982b697a3f7ac4a0
uupaa <uupaa.js@gmail.com>
2014-01-10 07:18:10 +0900

    fix some detection bugs.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,18 @@
 new Test().add([
-        testDeviceSpec,
+        testDevice,
+        testDeviceNexus5,
         testDeviceRevision_Nexus7_2013,
     ]).run();
 
-function testDeviceSpec(next) {
-//    var userAgent = "Mozilla/5.0 (Linux; Android 4.2.2; Nexus 7 Build/JDQ39) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.169 Safari/537.22";
-    var userAgent = "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36";
+function testDevice(next) {
+    var spec = Device();
 
+    console.log("testDevice ok: " + spec.DEVICE.TOKEN);
+    next && next.pass();
+}
+
+function testDeviceNexus5(next) {
+    var userAgent = "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36";
     var spec = Device(userAgent);
 
     if (spec.DEVICE.TOKEN   === "Nexus 5" &&
@@ -17,19 +23,17 @@ function testDeviceSpec(next) {
         spec.DISPLAY.DPR    === 3 &&
         spec.MEMORY.RAM     === 2 &&
         spec.GPU.TYPE       === "Adreno") {
-        console.log("testDeviceSpec ok");
+        console.log("testDeviceSpec ok: " + spec.DEVICE.TOKEN);
         next && next.pass();
     } else {
-        console.log("testDeviceSpec ng");
+        console.log("testDeviceSpec ng: " + spec.DEVICE.TOKEN);
         next && next.miss();
     }
 }
 
 function testDeviceRevision_Nexus7_2013(next) {
     var userAgent = "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JWR66N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.111 Safari/537.36";
-
     var spec = Device( userAgent );
-//alert(JSON.stringify(spec));
 
     if (spec.DEVICE.TOKEN     === "Nexus 7 (2013)" &&
         spec.DEVICE.SOC       === "APQ8064" &&
@@ -38,10 +42,10 @@ function testDeviceRevision_Nexus7_2013(next) {
         spec.DISPLAY.DPR      === 2 &&
         spec.MEMORY.RAM       === 2 &&
         spec.GPU.TYPE         === "Adreno") {
-        console.log("testDeviceRevision_Nexus7_2013 ok");
+        console.log("testDeviceRevision_Nexus7_2013 ok: " + spec.DEVICE.TOKEN);
         next && next.pass();
     } else {
-        console.log("testDeviceRevision_Nexus7_2013 ng. " + JSON.stringify(spec));
+        console.log("testDeviceRevision_Nexus7_2013 ng: " + spec.DEVICE.TOKEN);
         next && next.miss();
     }
 }
